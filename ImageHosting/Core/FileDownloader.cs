a896143104632813dd41ca88003f03e23cf4e11a
Nero Sule <sulenero@gmail.com>
2014-06-18 18:23:46 +0100

    Finishing up

--- ImageHosting/Core/FileDownloader.cs
+++ ImageHosting/Core/FileDownloader.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Net;
+using System.Security.AccessControl;
 using System.Web;
 using ImageHosting.Models;
 
@@ -23,7 +24,16 @@ namespace ImageHosting.Core
         {
             var path = HttpContext.Current.Server.MapPath("~/App_Data/" + _converter.ConvertLocation(obj.Id));
             if (!Directory.Exists(path))
+            {
                 Directory.CreateDirectory(path);
+                var info = new DirectoryInfo(path);
+                var security = info.GetAccessControl();
+                security.AddAccessRule(new FileSystemAccessRule("images.nerosule.net", FileSystemRights.FullControl,
+                    InheritanceFlags.ContainerInherit, PropagationFlags.None, AccessControlType.Allow));
+                security.AddAccessRule(new FileSystemAccessRule("images.nerosule.net", FileSystemRights.FullControl,
+                    InheritanceFlags.ObjectInherit, PropagationFlags.None, AccessControlType.Allow));
+                info.SetAccessControl(security);
+            }
             var extension = Path.GetExtension(obj.Link);
             if (string.IsNullOrWhiteSpace(extension))
                 extension = ".jpg";
